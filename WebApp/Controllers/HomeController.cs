b3c6583e39ff65983df3f8447a839c249e35de9b
Sumit Rawani <sumitrawani@gmail.com>
2018-12-18 16:56:14 -0500

    Changes for Application Insight

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.ApplicationInsights;
 
 namespace WebApp.Controllers
 {
@@ -19,16 +20,25 @@ namespace WebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            ViewBag.Message = "Oops" + CauseAnError();
+            return View(); 
         }
 
         public ActionResult Contact()
         {
+            var client = new TelemetryClient();
+            client.TrackEvent("Contact Information Was Requested");
             ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+
+        private int CauseAnError()
+        {
+            // Force an unhandled exception
+            System.Diagnostics.Trace.TraceWarning("Something bad is about to happen.");
+            var numerator = 0;
+            var denominator = 0;
+            return numerator / denominator;
         }
     }
 }
