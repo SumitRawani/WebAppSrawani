8211ecbd971a349c2a065ba45186808fffada18a
Sumit Rawani <sumitrawani@gmail.com>
2018-12-18 16:27:48 -0500

    Call google.com

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,8 +9,11 @@ namespace WebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async System.Threading.Tasks.Task<ActionResult> Index()
         {
+            var s = new HttpClient();
+            await s.GetAsync("http://google.com");
+
             return View();
         }
 
